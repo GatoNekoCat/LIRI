5644718e057553fc9ed664742ec9dd7ea0a71820
Raiicon <orankai@gmail.com>
2018-12-05 14:23:46 -0500

    added if statements for command logic structure

--- liri.js
+++ liri.js
@@ -1,23 +1,44 @@
 // dotenv loads from env file, allowing for obfuscated keys
 require('dotenv').config();
 
-
+// loading in my keys, axios for i/o, spotify, request, moment and fs
 const keys = require('./keys');
-
 const axios = require('axios');
-
 const Spotify = require('node-spotify-api');
-
 const request = require('request');
-
 const moment = require('moment');
-
 const fs = require('fs');
 
-let spotify = new Spotify(key.spotify);
+// adding keys to spotify.
+let spotify = new Spotify(keys.spotify);
+
 
 var liriReq = process.argv[2];
+var searchReq = "";
+
+for (var i=3;i<liriReq;i++) {
+    var str = process.argv[i];
+    if (i === 3) {
+        searchReq += str;
+    }
+    else {
+        searchReq += " " + str;
+    }
+}
+
+if (liriReq === "concert-this"){
+
+} else if (liriReq === "spotify-this-song"){
+
+} else if (liriReq === "movie-this"){
+
+} else if (liriReq === "do-what-it-says"){
+
+} else {
+    console.log("I DON'T UNDERSTAND!!!")
+}
 
 console.log(liriReq);
+console.log(searchReq);
 
 
