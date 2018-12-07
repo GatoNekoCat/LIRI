541e32db655f066c2a850401bdab686038bcb5ad
Raiicon <orankai@gmail.com>
2018-12-07 00:32:48 -0500

    final commit!

--- liri.js
+++ liri.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const keys = require('./keys');
 const axios = require('axios');
 const Spotify = require('node-spotify-api');
-const request = require('request');
 const moment = require('moment');
 const fs = require('fs');
 const inquirer = require('inquirer');
@@ -17,61 +16,130 @@ let spotify = new Spotify(keys.spotify);
 var searchReq = "";
 
 //This is called after the promise of searchWhat is fulfilled
-function concertFunc(){
+function concertFunc() {
     axios.get("https://rest.bandsintown.com/artists/" + searchReq + "/events?app_id=codingbootcamp").then(
-    function(response) {
-        console.log(response);
-        
-    });
+        function (response) {
+            console.log(response);
+
+        });
 };
 // Inquirer prompts, going to make it a choice type
 inquirer.prompt([{
     type: 'list',
     name: 'command',
     message: 'What would you like to do?',
-    choices: ['concert-this','spotify-this','movie-this','do-what-it-says']
-    },{
+    choices: ['concert-this', 'spotify-this', 'movie-this', 'do-what-it-says']
+}, {
     type: 'input',
     name: 'value',
     message: 'Enter a name:'
-    
-    
-}]).then(function(inquirerResponse){
+
+
+}]).then(function (inquirerResponse) {
     var responseCommand = inquirerResponse.command;
-    var promptValue = inquirerResponse.value.split(" ").join("%20");
-    console.log(promptValue);
-if (responseCommand === "concert-this"){
-    axios.get('https://rest.bandsintown.com/artists/' + promptValue + '/events?app_id=codingbootcamp&date=upcoming').then(
-        function(response){
-            var jsonData = response.data;
+    var promptValue = inquirerResponse.value;
+    // console.log('this here is the inquirerValue' + promptValue);
 
-            jsonData.forEach(e => {  
+    switch (responseCommand) {
+        case 'concert-this':
+            promptValue.split(" ").join("%20")
+            axios.get('https://rest.bandsintown.com/artists/' + promptValue + '/events?app_id=codingbootcamp&date=upcoming').then(
+                function (response) {
+                    var jsonData = response.data;
 
-            var venuData = [
-                "----------------------------------------",
-                "Venue: " + e.venue.name,
-                "Location: " + e.venue.city + " " + e.venue.region + " " + e.venue.country,
-                "Date: " + e.datetime,
-                "----------------------------------------",
+                    jsonData.forEach(e => {
+
+                        var venuData = [
+                            "----------------------------------------",
+                            "Venue: " + e.venue.name,
+                            "Location: " + e.venue.city + " " + e.venue.region + " " + e.venue.country,
+                            "Date: " + e.datetime,
+                            "----------------------------------------",
 
 
-            ].join("\n\n");
-            console.log(venuData);
-            fs.appendFile("log.txt", venuData,(error) => {})
-            });
+                        ].join("\n\n");
+                        console.log(venuData);
+                        fs.appendFile("log.txt", venuData, (error) => { })
+                    });
 
-        }
-        
-    )
+                }
 
-} else if (responseCommand === "spotify-this-song"){
+            )
+            break;
 
-} else if (responseCommand === "movie-this"){
+        case 'spotify-this':
 
-} else if (responseCommand === "do-what-it-says"){
+            spotify.search({ type: 'track', query: promptValue }, function (err, apiData) {
+                if (err) {
+                    return console.log('Error Has occured: ' + err);
+                }
+                var songs = apiData.tracks.items;
 
-} else {
-    console.log("I DON'T UNDERSTAND!!!")
-}
+
+                for (let i = 0; i < songs.length; i++) {
+                    console.log("Number: ", i, "/", songs.length);
+                    // console.log("Artist(s): ", songs[i].artists.map(getArtistName));
+                    console.log("Song Name: ", songs[i].name);
+                    console.log("Preview URL: ", songs[i].preview_url);
+                    console.log("Album: ", songs[i].album.name);
+                    console.log("--------------------------------");
+                }
+
+                const trackData = apiData.tracks.items[0];
+                var songData = [
+                    "----------------------------------------",
+                    trackData.name,
+                    "Song By: " + trackData.artists[0].name,
+                    "Album: " + trackData.album.name,
+                    "----------------------------------------",
+
+                ]
+                fs.appendFile("log.txt", songData, (error) => { });
+                // console.log(songData);
+            })
+            break;
+
+        case 'movie-this':
+            getMovie(promptValue);
+            break;
+        case 'do-what-it-says':
+            followFileCommands(promptValue);
+            break;
+        default:
+            console.log("LIRI doesn't know that command, please try again.");
+
+    };
+
 
 });
+
+function getMovie(movie){
+    if (movie === undefined || movie === " ") {
+        movie = 'Fargo';
+    };
+    movie.split(" ").join("+");
+
+    const queryURL = 'http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&apikey=trilogy';
+    axios.get(queryURL).then(function(result){
+        // console.log(result);
+        console.log('Title: ' + result.data.Title);
+        console.log('Rated: ' + result.data.Rated);
+        console.log('Year: ' + result.data.Year);
+        console.log('IMDB Rating: ' + result.data.imdbRating);
+        console.log('Country: ' + result.data.Country);
+        console.log('Language: ' + result.data.Language);
+        console.log('Plot: ' + result.data.Plot);
+        console.log('Actors: ' + result.data.Actors);
+        console.log("------------------------------------------------");
+    });
+
+}
+
+function followFileCommands (fileToRead) {
+    fs.readFile(fileToRead,"utf8",(err,data)=>{
+        if(err)throw err;
+
+        var readData = data;
+        console.log(readData);
+    })
+}
